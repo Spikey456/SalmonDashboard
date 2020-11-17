da974cb93d79ce6ebf0d9662ebaa2bd3d745133e
Spikey456 <63517093+Spikey456@users.noreply.github.com>
2020-11-17 23:18:29 +0800

    removed conflict messages

--- product.php
+++ product.php
@@ -5,11 +5,7 @@
 <html >
 <head>
 <meta charset="utf-8">
-<<<<<<< Updated upstream
-	
-=======
   
->>>>>>> Stashed changes
 	<script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap4.min.css">
